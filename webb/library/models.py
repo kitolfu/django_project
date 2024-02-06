3c3593834a0f424c8cc3f2ce1a2ef212014626c8
kitolfu <jds.xx17@gmail.com>
2024-02-06 22:51:58 +0300

    commit2

--- webb/library/models.py
+++ webb/library/models.py
@@ -12,7 +12,10 @@ class Genre(models.Model):
     )
 
 class Author(models.Model):
-    name = models.CharField(max_length=20, help_text='Введите имя автора')
+    name = models.CharField(max_length=20,
+        verbose_name='Имя',                     
+        help_text='Введите имя автора')
+    
     
     def __str__(self):
         return self.name
@@ -20,7 +23,8 @@ class Author(models.Model):
 class Book(models.Model):
     name = models.CharField(
         verbose_name = 'Название',
-        max_length=25
+        max_length=25,
+        help_text='Введите имя автора'
     )
     price = models.IntegerField(
         verbose_name='"Цена',
@@ -32,28 +36,34 @@ class Book(models.Model):
         related_name='books',
         verbose_name='Жанр',
         on_delete=models.PROTECT,
+        help_text='Введите название жанра'
     )
     author  = models.ForeignKey(
         Author,
         help_text='Выберите автора',
         verbose_name='Автор',
         on_delete=models.PROTECT,
+        related_name='books'
     )
     creation_date = models.DateField(
         verbose_name='Дата',
+        help_text='Введите дату создания'
     )  
     description = models.TextField(
         verbose_name='Описание',
         null=True,
+        help_text='Введие описание книги'
     )
     pages_number = models.IntegerField(
         verbose_name='Страницы',
+        help_text='Введите количество страниц'
     )
 
     
     # Метаданные
     class Meta:
         verbose_name = "Book"
+        ordering = ['name', 'author', 'creation_date']
 
 
 
